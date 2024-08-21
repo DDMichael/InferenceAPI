64f5f70a063b03660504bf72000d99c9f5be84a2
DDMichael <dmichael114@gmail.com>
2024-08-21 22:37:04 +0800

    initial commit

---
+++ pythonProject/init.py
@@ -0,0 +1,17 @@
+import json
+
+from recorder import live_speech
+
+wakeup_words = []
+
+for i in range(5):
+    print("Please say the activation key phrase")
+    for phrase in live_speech():
+        print(f"Heard '{str(phrase)}'\n")
+        wakeup_words.append(phrase)
+        break
+
+with open("wakeup_words.json", "w") as f:
+    json.dump(list(set(wakeup_words)), f)
+
+print("Recognition finished")
